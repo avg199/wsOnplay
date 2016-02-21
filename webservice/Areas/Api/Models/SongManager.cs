04e4b18a53c0a90ef8045af76f9cbdb53092bd59
Albert Vidal <albert.vidal@outlook.es>
2016-02-21 17:26:37 +0100

    Models

---
+++ webservice/Areas/Api/Models/SongManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace webservice.Areas.Api.Models
+{
+    class SongManager{
+        private static string cadenaConexio = @"Data Source=bdonplay.database.windows.net;Initial Catalog=DBonplay;Integrated Security=True";
+        
+        public song obtenirSong (int id){
+
+            song sng = null;
+
+            SqlConnection con = new SqlConnection(cadenaConexio);
+
+            con.Open();
+
+            string sql = "SELECT Nom, Duracio FROM Songs WHERE IdSong = @idSong";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            cmd.Parameters.Add("@idsong", System.Data.SqlDbType.NVarChar).Value = id;
+
+            SqlDataReader reader =
+                cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+
+            if (reader.Read())
+            {
+                sng = new song();
+                sng.Id = id;
+                sng.Nom = reader.GetString(0);
+                sng.Duracio = reader.GetInt32(1);
+            }
+
+            reader.Close();
+
+            return sng;
+        }
+        
+        public List<song> obtenirSongs(){
+
+            List<song> llista = new List<song>();
+            SqlConnection con = new SqlConnection(cadenaConexio);
+            con.Open();
+
+            string sql = "SELECT IdSong, Nom, Duracio FROM Songs";
+            SqlCommand cmd = new SqlCommand(sql,con);
+
+            SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+
+            while (reader.Read())
+            {
+                song sng = new song();
+
+                sng = new song();
+                sng.Id = reader.GetInt32(0);
+                sng.Nom = reader.GetString(1);
+                sng.Duracio = reader.GetInt32(2);
+
+                llista.Add(sng);
+            }
+
+            reader.Close();
+
+            return llista;
+        }
+    }
+
+
+    }
+    }
+}
---
+++ webservice/Areas/Api/Models/song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webservice.Areas.Api.Models
+{
+    class song{
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Duracio { get; set; }
+    }
+}
